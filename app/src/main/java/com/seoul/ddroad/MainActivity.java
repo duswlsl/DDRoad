3f16761ba3b1c5469e1215c95308714738d84e68
guitarhyo <guitarhyo@gmail.com>
2018-08-15 23:03:15 +0900

    버튼 추가및 인트로 액티비티 추가

--- app/src/main/java/com/seoul/ddroad/MainActivity.java
+++ app/src/main/java/com/seoul/ddroad/MainActivity.java
@@ -1,7 +1,12 @@
 package com.seoul.ddroad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.seoul.ddroad.intro.IntroActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
         //메인
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        IntroActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
     }
 }
--- app/src/main/java/com/seoul/ddroad/intro/IntroActivity.java
+++ app/src/main/java/com/seoul/ddroad/intro/IntroActivity.java
@@ -1,7 +1,20 @@
 package com.seoul.ddroad.intro;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.seoul.ddroad.R;
+
 /**
  * Created by guitarhyo on 2018-08-15.
  */
-public class IntroActivity {
+public class IntroActivity  extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
 }
+
