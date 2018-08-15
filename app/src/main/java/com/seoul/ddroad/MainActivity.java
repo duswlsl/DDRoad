2853d51e533749998cd56bb0aea0280d25159f18
guitarhyo <guitarhyo@gmail.com>
2018-08-16 00:42:55 +0900

    메뉴 연결 추가

--- app/src/main/java/com/seoul/ddroad/MainActivity.java
+++ app/src/main/java/com/seoul/ddroad/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.seoul.ddroad.board.BoardActivity;
+import com.seoul.ddroad.diary.DiaryActivity;
 import com.seoul.ddroad.intro.IntroActivity;
+import com.seoul.ddroad.map.MapActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +20,47 @@ public class MainActivity extends AppCompatActivity {
         //메인
         setContentView(R.layout.activity_main);
 
-        Button b = (Button)findViewById(R.id.button1);
-        b.setOnClickListener(new View.OnClickListener() {
+        Button b1 = (Button)findViewById(R.id.button1);
+        Button b2 = (Button)findViewById(R.id.button2);
+        Button b3 = (Button)findViewById(R.id.button3);
+        Button b4 = (Button)findViewById(R.id.button4);
+
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(
                         getApplicationContext(), // 현재 화면의 제어권자
                         IntroActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        MapActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        DiaryActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        BoardActivity.class); // 다음 넘어갈 클래스 지정
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
