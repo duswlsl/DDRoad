8d15f6a88e7705e7e8c3b3721019f03a36b55a4e
hwang-kyowook <hkw0101@gmail.com>
2018-08-16 01:45:19 +0900

    액티비티 인트로 클래스 인트로 액티비티 전환 수정

--- app/src/main/java/com/seoul/ddroad/intro/IntroActivity.java
+++ app/src/main/java/com/seoul/ddroad/intro/IntroActivity.java
@@ -1,8 +1,11 @@
 package com.seoul.ddroad.intro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.seoul.ddroad.MainActivity;
 import com.seoul.ddroad.R;
 
 /**
@@ -10,11 +13,31 @@ import com.seoul.ddroad.R;
  */
 public class IntroActivity  extends Activity
 {
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 4000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
     }
 }
 
