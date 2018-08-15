7ac22bf9e87e591f6e836ca2b7cccfb62cd62777
guitarhyo <guitarhyo@gmail.com>
2018-08-15 18:53:31 +0900

    패키지 추가2

---
+++ app/src/main/java/com/seoul/ddroad/diary/DiaryActivity.java
@@ -0,0 +1,7 @@
+package com.seoul.ddroad.diary;
+
+/**
+ * Created by guitarhyo on 2018-08-15.
+ */
+public class DiaryActivity {
+}
---
+++ app/src/main/java/com/seoul/ddroad/map/MapActivity.java
@@ -0,0 +1,7 @@
+package com.seoul.ddroad.map;
+
+/**
+ * Created by guitarhyo on 2018-08-15.
+ */
+public class MapActivity {
+}
