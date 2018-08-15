3b6c6d0e7563270ede3905c5db85d05db82d7c0d
guitarhyo <guitarhyo@gmail.com>
2018-08-15 18:50:56 +0900

    패키지 추가

--- app/src/main/java/com/seoul/ddroad/MainActivity.java
+++ app/src/main/java/com/seoul/ddroad/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //메인
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/seoul/ddroad/intro/IntroActivity.java
@@ -0,0 +1,7 @@
+package com.seoul.ddroad.intro;
+
+/**
+ * Created by guitarhyo on 2018-08-15.
+ */
+public class IntroActivity {
+}
