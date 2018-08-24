6d129167fcdf4606dec84f58b5f9e45a3cb8f131
guitarhyo <guitarhyo@gmail.com>
2018-08-24 18:10:14 +0900

    레이아웃 잡아보기

--- app/src/main/java/com/seoul/ddroad/diary/DiaryActivity.java
+++ app/src/main/java/com/seoul/ddroad/diary/DiaryActivity.java
@@ -5,16 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.seoul.ddroad.R;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by guitarhyo on 2018-08-15.
  */
 public class DiaryActivity extends AppCompatActivity {
+    private Date mCurrentDate;
+    //private CaldroidFragment calendarFragment;
+
     @Override
     public void onCreate( Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
+
+        mCurrentDate = Calendar.getInstance().getTime();
+        Calendar cal = Calendar.getInstance();
+        Date currentDate = cal.getTime();
 
     }
 }
