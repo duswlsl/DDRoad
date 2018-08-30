194b5cdd016b266ae365d71c5d98df21643b3f99
guitarhyo <guitarhyo@gmail.com>
2018-08-30 22:57:20 +0900

    미세 연결 추가 버튼

--- app/src/main/java/com/seoul/ddroad/AppCustom.java
+++ app/src/main/java/com/seoul/ddroad/AppCustom.java
@@ -6,9 +6,9 @@ public class AppCustom extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/nanumbrush.ttf");
-        FontsOverride.setDefaultFont(this, "MONOSPACE", "fonts/nanumbrush.ttf");
-        FontsOverride.setDefaultFont(this, "SERIF", "fonts/nanumbrush.ttf");
-        FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/nanumbrush.ttf");
+        FontsOverride.setDefaultFont(this, "DEFAULT", "font/nanumbrush.ttf");
+        FontsOverride.setDefaultFont(this, "MONOSPACE", "font/nanumbrush.ttf");
+        FontsOverride.setDefaultFont(this, "SERIF", "font/nanumbrush.ttf");
+        FontsOverride.setDefaultFont(this, "SANS_SERIF", "font/nanumbrush.ttf");
     }
 }
