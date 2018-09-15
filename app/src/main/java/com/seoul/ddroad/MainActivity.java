3d1f5a06c94b7f39fa8d95331845c4bed2c35b0a
guitarhyo <guitarhyo@gmail.com>
2018-09-15 14:53:55 +0900

    상태 이미지 등록

--- app/src/main/java/com/seoul/ddroad/MainActivity.java
+++ app/src/main/java/com/seoul/ddroad/MainActivity.java
@@ -1,5 +1,6 @@
 package com.seoul.ddroad;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,9 +8,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.seoul.ddroad.board.BoardActivity;
 import com.seoul.ddroad.board.BoardFragment;
+import com.seoul.ddroad.diary.DiaryActivity;
 import com.seoul.ddroad.diary.DiaryFragment;
 import com.seoul.ddroad.intro.DustFragment;
+import com.seoul.ddroad.intro.IntroActivity;
+import com.seoul.ddroad.intro.dustActivity_cool;
 import com.seoul.ddroad.map.MapFragment;
 
 import butterknife.ButterKnife;
@@ -23,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         callFragment(1);
         ButterKnife.bind(this);
+
+
+        Button button1 = (Button) findViewById(R.id.btn_intro) ;
+        button1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),IntroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button2 = (Button) findViewById(R.id.btn_board2) ;
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),BoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button3 = (Button) findViewById(R.id.btn_diary2) ;
+        button3.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),DiaryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @OnClick({R.id.btn_dust, R.id.btn_map, R.id.btn_diary, R.id.btn_board})
@@ -41,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
             case 2:
                 MapFragment fragment2 = new MapFragment();
                 transaction.replace(R.id.fragment_container, fragment2);
+
                 break;
             case 3:
                 DiaryFragment fragment3 = new DiaryFragment();
