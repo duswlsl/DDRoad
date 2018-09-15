ae21b75c5806b1c9f976b319184c8c008b5b07df
duswlsl <hongy3@naver.com>
2018-09-15 11:26:21 +0900

    연진 merge

--- app/src/main/java/com/seoul/ddroad/map/MapFragment.java
+++ app/src/main/java/com/seoul/ddroad/map/MapFragment.java
@@ -1,13 +1,28 @@
 package com.seoul.ddroad.map;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.seoul.ddroad.R;
+
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -22,19 +37,34 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapClickListener {
     private GoogleMap googleMap;
     private LatLng SEOUL = new LatLng(37.56, 126.97);
     private MapView mapView;
     private LatLng curLatlng;
+    public ArrayList<LatLng> coordList;
+    private PolylineOptions polyOptions;
+    private LocationRequest locRequest;
+    private FusedLocationProviderClient fusedLocClient;
+    private LocationCallback locCallback;
+
 
     public MapFragment() {
 
     }
 
+    protected void createLocationRequest() {
+        locRequest = new LocationRequest();
+        locRequest.setInterval(1000);
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createLocationRequest();
     }
 
 
@@ -53,31 +83,73 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             mapView.onCreate(savedInstanceState);
             mapView.getMapAsync(this);
         }
+        locCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null)
+                    return;
+                for (Location location : locationResult.getLocations()) {
+                    Log.d("map2",String.valueOf(location.getLatitude()));
+                }
+                super.onLocationResult(locationResult);
+            }
+        };
+        fusedLocClient = LocationServices.getFusedLocationProviderClient(this.getContext());
     }
 
     @Override
     public void onStart() {
         super.onStart();
         mapView.onStart();
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocClient.requestLocationUpdates(locRequest, locCallback, null);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
+        startLocationUpdates();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+        fusedLocClient.removeLocationUpdates(locCallback);
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        if (!googleApiCheck())
+            return;
+
+
         setMarker();
+        //this.googleMap.setOnInfoWindowClickListener(this);
+        coordList = new ArrayList<LatLng>();
+        PolylineOptions polyOptions = new PolylineOptions();
+    }
+
+    private boolean googleApiCheck() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int code = api.isGooglePlayServicesAvailable(this.getContext());
+        if (code == ConnectionResult.SUCCESS)
+            return true;
+        else {
+            api.getErrorDialog(this.getActivity(), code, 0).show();
+            return false;
+        }
     }
 
     public void setMarker() {
@@ -86,6 +158,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         markerOptions.position(curLatlng);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLatlng, 15));
         googleMap.addMarker(markerOptions);
+
     }
 
     public void setLastKnownLoc(Context context) {
@@ -102,5 +175,44 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             curLatlng = SEOUL;
         Log.d("map", String.valueOf(curLatlng.latitude));
     }
+
+    public void drawPolyline() {
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.RED);
+        polylineOptions.width(5);
+        //polylineOptions.addall(polyOptions);
+        googleMap.addPolyline(polylineOptions);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        // 창 클릭
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        // 맵 클릭
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
 
 }
